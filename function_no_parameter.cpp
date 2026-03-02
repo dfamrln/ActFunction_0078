81d8840ad3d8ee63bae39ba3f9978f031805cd9d
dfamrln <mhdffa1@gmail.com>
2026-03-02 11:56:17 +0700

    menambahkan fungsi input panjang dan lebar

---
+++ function_no_parameter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void input() {
+    int panjang, lebar;
+    cout << "Masukkan panjang : ";
+    cin >> panjang;
+    cout << "Masukkan lebar : ";
+    cin >> lebar;
+}
