46e4cc100cf49c644dfd7f4dad0ad4198ea33f9f
dfamrln <mhdffa1@gmail.com>
2026-03-02 12:26:01 +0700

    Menambahkan fungsi input, proses, dan output luas lingkaran

--- actfunc.cpp
+++ actfunc.cpp
@@ -3,3 +3,19 @@ using namespace std;
 
 int jarijari;
 float phi = 3.14159;
+
+void input()
+{
+    cout << "Masukkan Jari-Jari :";
+    cin >> jarijari;
+}
+
+float luasLingkaran(int a, float b)
+{
+    return a * a * b;
+}
+
+void output()
+{
+    cout << "Hasilnya : " << luasLingkaran(jarijari, phi);
+}
